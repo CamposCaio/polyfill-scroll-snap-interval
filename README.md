39d84c6c41c6ce1decdba6da7988b370810d27a3
CamposCaio <camposcaioh@gmail.com>
2022-04-05 14:39:19 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# polyfill-scroll-snap-interval
+# Polyfill Scroll-Snap Interval
+
+Simple implementation of snap-scroll feature, using _setInterval()_ for better compatibility between browsers.
+
+Basically, the script watches the scroll position and performs scroll alignment only when the user stops scrolling.
+
+The script accepts different heights of sections as long as all sections are the same height.
---
+++ script.js
@@ -0,0 +1,48 @@
+let isUserScrolling = false;
+let scrollingDirection = "";
+let previousScrollPosition = 0;
+
+const firstSectionElement = document.getElementsByTagName("section")[0];
+let sectionHeight = firstSectionElement.scrollHeight;
+
+new ResizeObserver(() => {
+  sectionHeight = firstSectionElement.scrollHeight;
+}).observe(firstSectionElement);
+
+function animateScroll(currentScrollPosition) {
+  topDisalignment = currentScrollPosition % sectionHeight;
+  if (topDisalignment == 0) return;
+  if (scrollingDirection === "top") {
+    window.scrollBy({
+      top: -topDisalignment,
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
+    window.scrollBy({
+      top: sectionHeight - topDisalignment,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+}
+
+setInterval(watchScroll, 25);
+
+function watchScroll() {
+  const currentScrollPosition = window.pageYOffset;
+  if (isUserScrolling) {
+    if (currentScrollPosition == previousScrollPosition) {
+      isUserScrolling = false;
+      animateScroll(currentScrollPosition);
+    } else {
+      scrollingDirection =
+        currentScrollPosition - previousScrollPosition < 0 ? "top" : "bottom";
+    }
+  } else {
+    if (currentScrollPosition != previousScrollPosition) {
+      isUserScrolling = true;
+    }
+  }
+  previousScrollPosition = currentScrollPosition;
+}
